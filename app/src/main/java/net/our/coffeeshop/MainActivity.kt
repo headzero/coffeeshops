5769a3f04c86978f27cef87f3f3a473de6ac8fa9
young <youngblue-zero@daum.net>
2017-04-04 00:03:16 +0900

    firebase initialize app and defaultTest

--- app/src/main/java/net/our/coffeeshop/MainActivity.kt
+++ app/src/main/java/net/our/coffeeshop/MainActivity.kt
@@ -2,11 +2,39 @@ package net.our.coffeeshop
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var ref: DatabaseReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_put_data.setOnClickListener {
+            ref?.setValue(text_view.text.toString())
+        }
+
+        setFirebaseDB()
+    }
+
+    private fun setFirebaseDB() {
+        val database = FirebaseDatabase.getInstance()
+        ref = database.getReference("message")
+        ref?.addValueEventListener(valueEventListener)
+    }
+
+    val valueEventListener = object : ValueEventListener {
+        override fun onCancelled(error: DatabaseError?) {
+            Log.d("MAINACTIVITY", "Failed to read value.", error?.toException());
+        }
+
+        override fun onDataChange(dataSnapshot: DataSnapshot?) {
+            val value = dataSnapshot?.getValue(String::class.java)
+            Log.d("MAINACTIVITY", "Value is: " + value)
+        }
+
     }
 }
---
+++ app/src/main/java/net/our/coffeeshop/MainApplication.kt
@@ -0,0 +1,12 @@
+package net.our.coffeeshop
+
+import android.app.Application
+import com.google.firebase.FirebaseApp
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
